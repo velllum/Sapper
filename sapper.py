60f45d4913a41e26b8df1d12c9273bd99b59b6a1
velllum <velllum@yandex.ru>
2021-10-21 01:05:54 +0300

    Реализация функционал заполнения ячейки значениями, если рядом находится мина

--- sapper.py
+++ sapper.py
@@ -18,6 +18,7 @@ class Cells(int, Enum):
     EMPTY = 0
     MINE = 9
     NUM = 15
+
     VERTICAL = NUM
     HORIZON = NUM
     FIELD = VERTICAL * HORIZON
@@ -27,65 +28,67 @@ class Client:
     """- игрок, клиент, инициализация данных, оперирование данными"""
 
     def __init__(self):
-        self.level: int  # уровень сложности
-        self.matrix: list[list[int]]
+        self.level: int = 0  # уровень сложности
+        self.data: list[int] = []
+        self.matrix: list[list[int]] = []
 
-    def set_difficulty(self, comp: Type[Complex], st: str):
+    def set_difficulty(self, complex: Type[Complex], st: str):
         """- установить значение уровня сложности"""
 
-        if st == comp.EASY.name:
-            self.level = comp.EASY.value
+        for comp in complex:
+            if st == comp.name:
+                self.level = comp.value
+                break
 
-        elif st == comp.MEDIUM.name:
-            self.level = comp.MEDIUM.value
-
-        elif st == comp.HARD.name:
-            self.level = comp.HARD.value
-
-    def create_data(self, cell: Type[Cells]) -> list[int]:
+    def create_data(self, cell_empty: int, cell_field: int, cell_mine: int):
         """- создать список с метками мин и пустых ячеек, перемешать рандомно"""
 
         # собрать в список значение 0 - пустое, 9 - мина
         lst = [
-            *[cell.EMPTY.value] * (cell.FIELD.value - self.level),
-            *[cell.MINE.value] * self.level
+            *[cell_empty] * (cell_field - self.level),
+            *[cell_mine] * self.level
         ]
 
         # перемешать список рандомно
         random.shuffle(lst)
 
-        return lst
+        self.data = lst
 
-    def create_matrix(self, cell: Type[Cells], lst: list) -> list[list[int]]:
+    def create_matrix(self, cell_field: int, cell_num: int):
         """- создать матрицу 15X15"""
 
-        # получить вложенные списки по размеру равномерной матрицы 15/15
-        return [
-            lst[cl: cl + cell.NUM.value]
-            for cl in range(0, cell.FIELD.value, cell.NUM.value)
+        # получить вложенные списки по размеру равномерной матрицы 15X15
+        lst = [
+            self.data[cl: cl + cell_num]
+            for cl in range(0, cell_field, cell_num)
         ]
 
+        self.matrix = lst
+
     def select_cells(self):
-        """- выделить ячейки, указав количество рядом находящихся мин, если есть близ мина"""
+        """- поднять значение ячейки, если рядом есть мины"""
+
 
     def init_client(self, str_level: str):
         """- инициализация данных клиента, сбор данных"""
 
         # установить уровень сложности
         self.set_difficulty(
-            comp=Complex,
-            st=str_level
+            complex=Complex,
+            st=str_level,
         )
 
-        # создать перемешанный список пустых и значений с данными растоновки мин
-        data = self.create_data(
-            cell=Cells,
+        # создать перемешанный список пустых и значений с установки мин
+        self.create_data(
+            cell_empty=Cells.EMPTY.value,
+            cell_field=Cells.FIELD.value,
+            cell_mine=Cells.MINE.value,
         )
 
         # создать матрицу
-        matrix = self.create_matrix(
-            cell=Cells,
-            lst=data
+        self.create_matrix(
+            cell_field=Cells.FIELD.value,
+            cell_num=Cells.NUM.value,
         )
 
 
@@ -130,8 +133,6 @@ class Manager:
 
 if __name__ == '__main__':
 
-    print(Cells.MINE.value)
-
     lst = [
         *[Cells.EMPTY.value] * (Cells.FIELD - Complex.EASY),
         *[Cells.MINE.value] * Complex.EASY
@@ -139,12 +140,32 @@ if __name__ == '__main__':
 
     random.shuffle(lst)
 
-    lst_field = [
+    cls = [
         lst[cell: cell + Cells.NUM]
         for cell in range(0, Cells.FIELD, Cells.NUM)
     ]
 
-    for i in lst_field:
-        print(i, len(i))
+    for row in range(Cells.NUM.value):
+
+        # print(cls[row])
+
+        for col in range(Cells.NUM.value):
+
+            if cls[row][col] == Cells.MINE.value:
+
+                if not col == (15 - 1) and cls[row][col + 1] != 9:
+                    cls[row][col + 1] = cls[row][col + 1] + 1
+
+                if not col == 0 and cls[row][col - 1] != 9:
+                    cls[row][col - 1] = cls[row][col - 1] + 1
+
+                if not row == 0 and cls[row - 1][col] != 9:
+                    cls[row - 1][col] = cls[row - 1][col] + 1
+
+                if not row == (15 - 1) and cls[row + 1][col] != 9:
+                    cls[row + 1][col] = cls[row + 1][col] + 1
+
+    for i in cls:
+        print(i)
 
 
