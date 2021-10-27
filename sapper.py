371b9ae161e77d27eefb501f980cca80e2d0df23
velllum <velllum@yandex.ru>
2021-10-28 00:37:24 +0300

    изменить рандомную установку мин, через объект

--- sapper.py
+++ sapper.py
@@ -33,32 +33,40 @@ class Values(int, Enum):
 
 
 class Cells:
-    """- игрок, клиент, инициализация данных, оперирование данными"""
+    """- ячейки"""
 
-    count = Values.EMPTY.value
+    count: int = Values.EMPTY.value
 
     def __init__(self, row, column):
         self.mine: bool = False  # мина или нет
         self.row: int = row  # номер строки
         self.column: int = column  # номер колонки
-        self.number: int = Cells.get_number()  # порядковый номер
-        self.count_near_mine: int = 0  # количество мин находящиеся рядом
+        self.id: int = Cells.get_id()  # порядковый номер
+        self.count_mine: int = Values.EMPTY.value  # количество мин находящиеся рядом
 
     def __repr__(self):
-        return f"<{self.row}.{self.column}.{self.number}> <{self.mine}>"
+        # return f"<{self.row}.{self.column}.{self.id}> <{self.mine}> <{self.count_mine}>"
+        return f"<{self.mine}>"
+
+    def set_mine(self):
+        """- установить мину"""
+
+        self.mine = True
 
     @staticmethod
-    def get_number():
+    def get_id():
+        """- получить идентификационный номер ячейки"""
+
         Cells.count += 1
         return Cells.count
 
 
 class Field:
-    """- реализация заполнения матрицы"""
+    """- поле"""
 
     def __init__(self):
         self.level: int = 0  # уровень сложности
-        self.field: list[list[int]] = []
+        self.cells: list[list[Cells]] = []
 
     def set_difficulty(self, cxs: Type[Complex], st: str):
         """- установить значение уровня сложности"""
@@ -68,29 +76,46 @@ class Field:
                 self.level = cx.value
                 break
 
-    def create(self, size: int):
+    def create(self, horizon: range, vertical: range):
         """- создать матрицу 15X15"""
 
-        # получить вложенные списки по размеру равномерной матрицы 15X15
         lst = [
             [
+                # добавляем объект ячейки в матрицу
                 Cells(row=row, column=col)
-                for col in range(size)
+                for col in vertical
             ]
-            for row in range(size)
+            for row in horizon
         ]
 
-        self.field = lst
+        self.cells = lst
 
-    def fill(self, grid: list, horizon: range, vertical: range, first: int, second: int, mine: int, ratio: int):
-        """- заполнить матрицу по диагонали сверху и снизу"""
+    def place_mines(self, size: int):
+        """- расставить мины"""
+
+        # получить список с id, уникальными идентификаторами объекта
+        lst = [cl.id for cel in self.cells for cl in cel]
+
+        # перемешать список с id рандомно
+        random.shuffle(lst)
+
+        # перезаписать матрицу, расставить мины,
+        # используя перемешанный рандомно список и обрезанный по указателю сложности,
+        # полученному от пользователя
+        for cel in self.cells:
+            for cl in cel:
+                if cl.id in lst[: size]:
+                    cl.set_mine()
+
+    def fill_count_mine_nearby(self, grid: list, horizon: range, vertical: range, first: int, second: int, mine: int, ratio: int):
+        """- заполнить матрицу количеством мин по соседству"""
 
         # создать индексы строк и столбцов для обхода матрицы
         for rows in horizon:
             for columns in vertical:
 
                 # найти мину, для добавления значений соседним ячейкам
-                if self.field[rows][columns] == mine:
+                if self.cells[rows][columns] == mine:
 
                     # обходим соседние ячейки, используя значения из списка [-1, 0, 1]
                     for row_dx in grid:
@@ -108,19 +133,26 @@ class Field:
                                 continue
 
                             # проверяем значение в соседних ячейках на мины, при положительном результате пропускаем
-                            if self.field[rows + row_dx][columns + col_dx] == mine:
+                            if self.cells[rows + row_dx][columns + col_dx] == mine:
                                 continue
 
                             # если проверки проходят, то меняем значение в нашей матрице
-                            self.field[rows + row_dx][columns + col_dx] += ratio
+                            self.cells[rows + row_dx][columns + col_dx] += ratio
 
     def init_field(self, str_level: str):
         """- инициализация данных клиента, сбор данных"""
+
         # установить уровень сложности
         self.set_difficulty(cxs=Complex, st=str_level)
 
         # создать матрицу
-        self.create(size=Values.SIZE.value)
+        self.create(
+            horizon=range(Values.HORIZON.value),
+            vertical=range(Values.VERTICAL.value)
+        )
+
+        # расставить мины
+        self.place_mines(size=Values.SIZE.value)
 
 
 class Game:
@@ -139,19 +171,23 @@ class Game:
         """- поражения в игре"""
 
 
-class Manager:
-    """- точка входа"""
-
-
 if __name__ == '__main__':
 
     mx = Field()
     mx.init_field("EASY")
 
-    for mx in mx.field:
-        print(mx)
+    for cell in mx.cells:
+        # for cl in cell:
+        print(cell)
 
-    # cl = Cells(1, 2)
-    # print(cl)
+    print("=" * 50)
+
+
+
+    # print(mx.place_mines())
+
+    # for cell in mx.cells:
+    #     for cl in cell:
+    #         print(cl.count)
 
 
