a0371d0944b88f005c5106ca0c563a5df7596f6c
velllum <velllum@yandex.ru>
2021-11-15 00:47:25 +0300

    Рефакторинг кода, изменение структуры проекта

--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,7 @@ def create_app(path: str) -> Flask:
     """- создать фабрику flask"""
     app = Flask(__name__)
 
-    register_game(app)
+    register_resources(app)
     register_config(app, path)
 
     # запускаем контекст объекта,
@@ -20,7 +20,7 @@ def create_app(path: str) -> Flask:
         return app
 
 
-def register_game(app: Flask):
+def register_resources(app: Flask):
     """- регистрируем игру"""
     app.gm = Game()
 
